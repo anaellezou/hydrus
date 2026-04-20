1285609390e79f4f06da7c38eedfee9ffb6c413a
anaellezou <anaelle.zouari@gmail.com>
2026-04-20 18:08:02 +0200

    fix(backend/entrypoint.sh): temporary fix, remove jlpt.db directory if it exists since its an error

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-# Create database if it doesn't exist
+# temporary fix : remove jlpt.db directory if it exists, because it should be a file, not a directory.
+# This can happen if the previous run of the container failed to create the database file and created a directory instead.
+if [ -d /app/jlpt.db ]; then
+    echo "Removing invalid jlpt.db directory..."
+    rm -rf /app/jlpt.db
+fi
+
+# Create db if it doesn't exist
 if [ ! -f /app/jlpt.db ]; then
     echo "Creating database..."
     python app/database/create_db.py
